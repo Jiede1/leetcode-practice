c54e0ec9a7d3ee30c63d0ed9a1434755043871fc
jiede1 <3081881935@qq.com>
2018-11-23 00:54:27 +0800

    leetcode 15

--- src/src_Jiede1/ThreeSum.java
+++ src/src_Jiede1/ThreeSum.java
@@ -10,6 +10,38 @@
 
 package src_Jiede1;
 
-public class ThreeSum {
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class ThreeSum {
+	public List<List<Integer>> threeSum(int[] nums) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++) {
+            if(i>0&&nums[i]==nums[i-1]) {
+        		continue;
+        	    }
+        	int start=i+1;
+        	int end=nums.length-1;
+        	while(start<end) {
+        		if(start > i + 1 && nums[start - 1] == nums[start]) {
+        			start++;
+        			continue;
+        		}
+        		if(nums[start]+nums[end]>-nums[i]) {
+        			end--;
+        		}
+        		else if(nums[start]+nums[end]<-nums[i]) {
+        			start++;
+        		}
+        		else {
+        			result.add(Arrays.asList(nums[i],nums[start],nums[end]));
+        			start++;
+                    end--;
+        		}
+        	}
+        }
+        return result;
+    }
 }
