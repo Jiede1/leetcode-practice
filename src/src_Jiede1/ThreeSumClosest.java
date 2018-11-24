5bfbac462c2338b79a56ce99d28fcb91246e16dd
jiede1 <3081881935@qq.com>
2018-11-24 23:03:59 +0800

    leetcode 16

--- src/src_Jiede1/ThreeSumClosest.java
+++ src/src_Jiede1/ThreeSumClosest.java
@@ -7,9 +7,30 @@
 * @version 1.0  
 */  
 
-
 package src_Jiede1;
 
-public class ThreeSumClosest {
+import java.util.Arrays;
 
+public class ThreeSumClosest {
+	public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest=nums[0]+nums[1]+nums[2];
+        int diff=Math.abs(target-closest);
+        if(diff==0)return closest;
+        for(int i=0;i<nums.length-2;i++) {
+        	int begin=i+1;
+        	int end=nums.length-1;
+        	while(end>begin) {
+        		int temp=nums[i]+nums[begin]+nums[end];	
+        		if(Math.abs(temp-target)<diff) {
+        			diff=Math.abs(temp-target);
+        			closest=temp;
+        		}
+        		if(temp<target)begin++;
+        		else if(temp>target) end--;
+        		else return target;
+        	}
+        }
+        return closest;
+    }
 }
