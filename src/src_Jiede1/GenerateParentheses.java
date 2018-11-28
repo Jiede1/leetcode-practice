34812aad8047b7a60172ba4698c42cbabf5949f9
Jiede1 <3081881935@qq.com>
2018-11-29 01:04:03 +0800

    feat:leetcode 22 digui

---
+++ src/src_Jiede1/GenerateParentheses.java
@@ -0,0 +1,47 @@
+/**  
+* Title: GenerateParentheses.java
+* Description:   
+* Copyright: Copyright (c) 2018  
+* @author Jiede1  
+* @date 2018Äê11ÔÂ28ÈÕ  
+* @version 1.0  
+*/  
+
+package src_Jiede1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+
+public class GenerateParentheses {
+	public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        doadd(n,n,list,"");
+        return list;    
+    }
+    
+    public static void doadd(int left, int right, List<String> list, String path){
+    	//System.out.println(path);
+    	
+        if (left == 0 && right == 0){
+            list.add(path);
+            System.out.println(list);
+            return;
+        }
+        
+        if (left != 0)
+            doadd(left-1,right,list,path+"(");
+        /*
+        for(String l:list) {
+        	System.out.println(l);
+        }
+        */
+        if (right != 0 && right > left)
+            doadd(left,right-1,list,path+")");
+    }
+    public static void main(String[] args) {
+    	GenerateParentheses aGenerateParentheses=new GenerateParentheses();
+    	aGenerateParentheses.generateParenthesis(2);
+    }
+}
