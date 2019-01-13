68377bd172132846a38ff6146c1eac7045d9c02f
Jiede1 <3081881935@qq.com>
2019-01-13 14:57:47 +0800

    leetcode 119

---
+++ src/easy/pascalTriII.java
@@ -0,0 +1,42 @@
+/**  
+* Title: pascalTriII.java
+* Description:   
+* Copyright: Copyright (c) 2018  
+* @author Jiede1  
+* @date 2019Äê1ÔÂ13ÈÕ  
+* @version 1.0  
+*/  
+
+
+package easy;
+import java.util.*;
+
+import com.sun.media.sound.SoftSynthesizer;
+
+public class pascalTriII {
+	public void printRow(List<Integer> List) {
+		for(int i=0;i<List.size();i++) {
+			System.out.print(List.get(i)+"  ");
+		}
+		System.out.println();
+	}
+	public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>(rowIndex + 1);
+        System.out.println(row.size());
+        for (int i = 0; i <= rowIndex; i++) {
+            row.add(1);
+            //printRow(row);
+            //System.out.println(row.size());
+            for (int j = i - 1; j >= 1; j--) { // From end to start
+                row.set(j, row.get(j) + row.get(j - 1));
+                printRow(row);
+                //System.out.println(j);
+            }
+        }
+        return row;
+    }
+	public static void main(String[] args) {
+		pascalTriII aIi =new pascalTriII();
+		System.out.println(aIi.getRow(4));
+	}
+}
