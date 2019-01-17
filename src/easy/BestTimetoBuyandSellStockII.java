263117012e2d52ab5eae453624d93f8fd6db4c4e
Jiede1 <3081881935@qq.com>
2019-01-17 21:15:33 +0800

    leetcode 122

--- src/easy/BestTimetoBuyandSellStockII.java
+++ src/easy/BestTimetoBuyandSellStockII.java
@@ -11,5 +11,13 @@
 package easy;
 
 public class BestTimetoBuyandSellStockII {
-
+	public int maxProfit(int[] prices) {
+        int res=0;
+        for(int i=0;i<prices.length-1;i++){
+            if(prices[i]<prices[i+1]){
+                res+=prices[i+1]-prices[i];
+            }
+        }
+        return res;
+    }
 }
