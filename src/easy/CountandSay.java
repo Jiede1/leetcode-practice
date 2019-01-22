5aff11e5532dab5e3b8e68e037c0ffabec834e9c
Jiede1 <3081881935@qq.com>
2019-01-22 22:39:41 +0800

    leetcode 38. Count and Say

---
+++ src/easy/CountandSay.java
@@ -0,0 +1,41 @@
+/**  
+* Title: CountandSay.java
+* Description:   
+* Copyright: Copyright (c) 2018  
+* @author Jiede1  
+* @date 2019Äê1ÔÂ22ÈÕ  
+* @version 1.0  
+*/  
+
+
+package easy;
+
+public class CountandSay {
+	public String countAndSay(int n) {
+		if(n==1) return "1";
+		String index ="1";
+		while(n>0) {
+			n--;
+			String temp = "";
+			int count = 1;
+			for(int k=0;k<index.length();k++) {
+				if(k+1<index.length() && index.charAt(k) == index.charAt(k+1)) {
+					count++;
+				}
+				else {
+					temp += String.valueOf(count)+index.charAt(k);
+					count=1;
+				}
+			}
+			index = temp;
+			System.out.println(temp);
+		}
+		return index;
+	}
+	
+	public static void main(String[] args) {
+		CountandSay aCountandSay = new CountandSay();
+		System.out.println(aCountandSay.countAndSay(4));
+				
+	}
+}
