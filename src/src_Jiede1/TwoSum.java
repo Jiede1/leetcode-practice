b1979cfebe90b776f3156e67beb031f3d8b1a1ff
Jiede1 <3081881935@qq.com>
2019-01-27 11:33:47 +0800

    update two sum

--- src/src_Jiede1/TwoSum.java
+++ src/src_Jiede1/TwoSum.java
@@ -3,6 +3,21 @@ package src_Jiede1;
 import java.util.Arrays;
 
 public class TwoSum {
+	/*
+	 * public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; ++i) {
+            if (m.containsKey(target - nums[i])) {
+                res[0] = i;
+                res[1] = m.get(target - nums[i]);
+                break;
+            }
+            m.put(nums[i], i);
+        }
+        return res;
+    }
+	 */
 	 public int[] twoSum(int[] nums, int target) {
 		 for (int i=0;i<nums.length-1;i++) {
 			 for (int j=i+1;j<nums.length;j++) {
