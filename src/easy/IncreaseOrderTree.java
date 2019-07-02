d94714a09725dd26e195935322a5c95473fbe19e
Jiede1 <3081881935@qq.com>
2019-07-02 23:40:42 +0800

    leetcode 897

---
+++ src/easy/IncreaseOrderTree.java
@@ -0,0 +1,15 @@
+/**  
+* Title: IncreaseOrderTree.java
+* Description:   
+* Copyright: Copyright (c) 2018  
+* @author Jiede1  
+* @date 2019Äê7ÔÂ2ÈÕ  
+* @version 1.0  
+*/  
+
+
+package easy;
+
+public class IncreaseOrderTree {
+
+}
